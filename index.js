9cd4f1e4727ca60565e9f00a503c1031e4199595
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-17 09:42:55 +0200

    creating and removing directories

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const fs = require('fs')
 
-const text = fs.readFileSync('readme.txt', 'utf-8')
+//  fs.readFile('readme.txt', 'utf-8', function(error, data){
+//     fs.writeFile('write.txt', data, function(error){
+//         if (error){
+//             console.log(error)
+//         }
+//     })
+// })
 
-fs.writeFileSync('write.txt', text)
+// fs.writeFileSync('write.txt', text)
+
+// fs.unlink('write.txt', (err) => {
+//     console.log(err)
+// })
+
+// fs.mkdir('my-folder', () => {
+//     fs.readFile('readme.txt', 'utf-8', (error, data) => {
+//         fs.writeFile('./my-folder/writeme.txt', data, () => {
+
+//         })
+//     })
+// })
+
+fs.unlink('./my-folder/writeme.txt', () => {
+    fs.rmdir('my-folder', (error) => {
+        console.log(error)
+    })
+})
