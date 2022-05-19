eb5864f0fc83d885154c151345af69642f2f6a4c
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-19 10:39:04 +0200

    started server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('hello world')
 })
+console.log('port 3000')
+server.listen(3000, '127.0.0.1')
