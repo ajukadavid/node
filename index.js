64197df7edd7d2ff81a5cefe5c2c755177af8cdc
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-20 20:24:41 +0200

    fetching data chunks

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 
-const readStream = fs.createReadStream(__dirname + '/readme.txt')
+const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8')
+
+readStream.on('data', (chunk) => {
+    console.log('new data recieved')
+    console.log(chunk)
+}) 
