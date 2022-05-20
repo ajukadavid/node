c584d6fa5e39c2c4700a65d861a27e4e6f13ca1d
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-20 16:18:03 +0200

    reading streams

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('hello world')
-})
-console.log('port 3000')
-server.listen(3000, '127.0.0.1')
+const readStream = fs.createReadStream(__dirname + '/readme.txt')
