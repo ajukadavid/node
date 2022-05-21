9efca002e7fd939e5978d17a3b951a43e3a38891
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-21 12:08:56 +0200

    reading chunks of data and writing to new file chunk by chunk

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require('http')
 const fs = require('fs')
 
 const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8')
+const writeStream = fs.createWriteStream(__dirname + '/write-me.txt')
 
 readStream.on('data', (chunk) => {
     console.log('new data recieved')
     console.log(chunk)
+    writeStream.write(chunk)
 }) 
