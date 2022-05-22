2f1fe063a3e72b04df28bc55f0e36d82ecd999bc
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-22 22:20:42 +0200

    piping data from a file to show on server

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require('http')
 const fs = require('fs')
 
-const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8')
-const writeStream = fs.createWriteStream(__dirname + '/write-me.txt')
 
-readStream.pipe(writeStream)
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/plain'
+    })
+    
+const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8')
+
+readStream.pipe(res)
+
+
+})
+
+server.listen(3000, '127.0.0.1')
