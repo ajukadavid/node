7e0bbb25f1e292a604f46653d24f89519b4e6754
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-22 22:16:51 +0200

    reading and writing streams with piping

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ const fs = require('fs')
 const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf-8')
 const writeStream = fs.createWriteStream(__dirname + '/write-me.txt')
 
-readStream.on('data', (chunk) => {
-    console.log('new data recieved')
-    console.log(chunk)
-    writeStream.write(chunk)
-}) 
+readStream.pipe(writeStream)
