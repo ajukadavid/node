f0288944b00930f72eeb43d647826956627315aa
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-23 22:05:24 +0200

    message

---
+++ helper.js
@@ -0,0 +1,17 @@
+const user = function(data){
+    return `${data} is logged in`
+}
+
+let id = function(id){
+    return `${id}`;
+}
+
+let email = function(email) {
+    return `${email}`
+}
+
+module.exports = {
+    user,
+    id,
+    email
+}
