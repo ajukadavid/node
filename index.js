304af2028bf3abc42cafc6a7b1a2746872812ca5
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-24 21:47:37 +0200

    accessing route parameters

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const app = express()
 app.get('/', (req, res) => {
 res.send('Home page')
 })
+app.get('/profile/:num', (req, res) => {
+    console.log(req.params)
+    
+
+    res.send('profile id is ' + req.params.num)
+    })
+
 app.listen(3000)
