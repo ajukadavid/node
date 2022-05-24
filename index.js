5d6809e58eea29c5cd6027a53f3c8ab4dd321da4
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-24 21:14:18 +0200

    setting up express

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
 
+const app = express()
 
-const server = http.createServer((req, res) => {
-   if(req.url === '/home' || req.url === '/') {
-       res.writeHead(200, {'Content-Type': 'text/html'})
-       fs.createReadStream(__dirname + '/index.html').pipe(res)
-   }
-  else if(req.url === '/about') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    fs.createReadStream(__dirname + '/about.html').pipe(res)
-} else {
-
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        fs.createReadStream(__dirname + '/404.html').pipe(res)
-   
-}
+app.get('/', (req, res) => {
+res.send('Home page')
 })
-
-server.listen(3000, '127.0.0.1')
+app.listen(3000)
