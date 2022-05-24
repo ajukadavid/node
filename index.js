8a2e3405f3720154388741040182803dbbd43cb4
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-25 01:46:39 +0200

    using ejs template engines

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
 
   res.sendFile(__dirname + '/index.html')
@@ -12,10 +14,9 @@ app.get("/about", (req, res) => {
     res.sendFile(__dirname + '/about.html')
 });
 
-app.get("/profile/:num", (req, res) => {
-  console.log(req.params);
-
-  res.send("profile id is " + req.params.num);
+app.get("/profile/:name", (req, res) => {
+    const data = {age: 30, job: 'developer'}
+ res.render('profile', {person: req.params.name, data})
 });
 
 app.listen(3000);
