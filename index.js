2d214857b043e5d55de6bc830f3d331a443e05f4
Ajuka David <ajuka.zephiniah@gmail.com>
2022-05-27 06:07:37 +0200

    using body parser to access form data

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
-
+const bodyparser = require('body-parser')
 const app = express();
+const urlencodedParser = bodyparser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs')
 
 app.use('/assets', express.static('assets'))
 
 app.get("/", (req, res) => {
-
   res.render('index')
-
 });
 
 app.get("/about", (req, res) => {
     res.render('about', {qs: req.query})
+});
+
+app.post("/about", urlencodedParser, (req, res) => {
+  console.log(req.body)
+  res.render('about', {qs: req.query})
 });
 
 app.get("/profile/:name", (req, res) => {
